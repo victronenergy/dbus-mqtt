c45cbd1c7a5907bf933d6e541eace2cae88eb7b0
Teun Lassche <teun@elnino.tech>
2020-02-07 10:08:51 +0100

    v1.16

--- dbus_mqtt.py
+++ dbus_mqtt.py
@@ -23,7 +23,7 @@ from mqtt_gobject_bridge import MqttGObjectBridge
 from mosquitto_bridge_registrator import MosquittoBridgeRegistrator
 
 
-SoftwareVersion = '1.15'
+SoftwareVersion = '1.16'
 ServicePrefix = 'com.victronenergy.'
 VeDbusInvalid = dbus.Array([], signature=dbus.Signature('i'), variant_level=1)
 blocked_items = {'vebus', u'/Interfaces/Mk2/Tunnel'}
