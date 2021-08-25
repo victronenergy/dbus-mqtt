940ce70fda60a0742ae03c03ad6eab51e2a67258
Izak Burger <isburger@gmail.com>
2021-08-25 09:12:08 +0200

    v1.26

--- dbus_mqtt.py
+++ dbus_mqtt.py
@@ -26,7 +26,7 @@ from mqtt_gobject_bridge import MqttGObjectBridge
 from mosquitto_bridge_registrator import MosquittoBridgeRegistrator
 
 
-SoftwareVersion = '1.25'
+SoftwareVersion = '1.26'
 ServicePrefix = 'com.victronenergy.'
 VeDbusInvalid = dbus.Array([], signature=dbus.Signature('i'), variant_level=1)
 blocked_items = {('vebus', u'/Interfaces/Mk2/Tunnel'), ('paygo', '/LVD/Threshold')}
